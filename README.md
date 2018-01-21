82e7d274dc4c025783756d179fef4958c94f395c
Hannah Hazi <hannah.hazi@cantab.net>
2018-01-21 12:16:46 +0000

    Partway done, let's put elsewhere so can play with somemore.

---
+++ README.md
@@ -0,0 +1,4 @@
+Kata from [sandromancuso](https://github.com/sandromancuso/social_networking_kata)
+
+To run in command line:
+$ python social_networking.py
--- social_networking.py
+++ social_networking.py
@@ -11,7 +11,7 @@ ALL_USERS_NAMES = os.path.join(USER_DATA_STORE, 'usernames.txt')
 
 
 def get_wallname(user):
-    """ this seems pointless, but we might alter it later"""
+    """ this seems pointless, but we might alter it later to add more info"""
     return user + '.txt'
 
 
@@ -22,16 +22,25 @@ def add_user(user):
         f.write(user)
 
     wall = get_wallname(user)
+
     with open(os.path.join(USER_DATA_STORE, wall), 'w+') as f:
         f.write("{}: {} joined the network".format(datetime.datetime.utcnow(),
                                                    user))
-
     return wall
+
+
+def read_from_wall(user):
+    """ Read from the wall of any given user
+    """
+    with open(get_wallname(user), 'r') as f:
+        wall_contents = f.readlines()
+    return wall_contents
 
 
 class SocialNetworkUser(object):
     def __init__(self, username):
         self.name = username
+        self.following = []
 
         with open(ALL_USERS_NAMES, 'r') as f:
             all_user_names = f.readlines()
@@ -45,6 +54,17 @@ class SocialNetworkUser(object):
         with open(self.user_data, 'a+') as f:
             f.write('{}: {}\n'.format(datetime.datetime.utcnow(), wall_text))
 
+    def add_follow(self, other_user_name):
+        with open(ALL_USERS_NAMES, 'r') as f:
+            all_user_names = f.readlines()
+            if other_user_name not in all_user_names:
+                return "Cannot follow non-existent user!"
+
+        if other_user_name not in self.following:
+            self.following.append(other_user_name)
+
+        return read_from_wall(other_user_name)
+
 
 class MyPrompt(cmd.Cmd):
     """ Prompt interacted with by the user to access  social network functions
@@ -53,6 +73,21 @@ class MyPrompt(cmd.Cmd):
         cmd.Cmd.__init__(self)
         self.prompt = '> '
         self.social_network_user = None
+
+    def precmd(self, line):
+        if '->' in line:
+            to_wall = line.split('->')[-1].strip(' ')
+            return 'wall_post ' + to_wall
+
+        elif 'follows' in line:
+            to_follow = line.split('follows')[-1]
+            return 'follow' + to_follow
+
+        elif 'wall' in line:
+            return 'wall_read ' + line.split('wall')[0]
+
+        else:
+            return line
 
     def do_quit(self, args):
         """ Quit. """
@@ -81,6 +116,15 @@ class MyPrompt(cmd.Cmd):
     def do_wall_post(self, wall_text):
         """ Post to wall"""
         self.social_network_user.post_to_wall(wall_text)
+
+    def do_follow(self, user_to_follow):
+        self.social_network_user.add_follow(user_to_follow)
+        self.do_wall_read(user_to_follow)
+
+    def do_wall_read(self, user):
+        users_wall = read_from_wall(user)
+        for line in users_wall:
+            print(line)
 
 
 if __name__ == "__main__":
